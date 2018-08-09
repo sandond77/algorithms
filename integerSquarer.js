bb79aac682eee3f3b6ccd47e731baf9b8dc4e89f
sandond77 <sandu@ucdavis.edu>
2018-08-09 14:32:03 -0700

    added prompt to integersquarer

--- integerSquarer.js
+++ integerSquarer.js
@@ -1,3 +1,5 @@
+//Use arrow function syntax to compute the square of only the positive integers (fractions are not integers) in the array realNumberArray and store the new array in the variable squaredIntegers.
+
 const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34];
 const squareList = (arr) => {
   "use strict";
