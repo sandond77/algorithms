763c0b6a3f7f92b95eaaababcb0761a4e7addd24
sandond77 <sandu@ucdavis.edu>
2018-09-30 20:20:36 -0700

    added base code for rover.js

--- odd.js
+++ odd.js
@@ -1,13 +1,30 @@
-function oddNumbers(l, r) {
-    let holder = [];
-    for(let i=l; i <= r; i++){
-        if(i % 2 !== 0 ){
-           holder.push(i);
-        }
-    }
-    console.log(holder)
-}
+// function oddNumbers(l, r) {
+//     let holder = [];
+//     for(let i=l; i <= r; i++){
+//         if(i % 2 !== 0 ){
+//            holder.push(i);
+//         }
+//     }
+//     console.log(holder)
+// }
 
 
 
-oddNumbers(3,9)
+// oddNumbers(3,9)
+
+// var y;
+
+// var test = (x) => {
+// 	var z = 5;
+// 	z += 2;
+// 	y =+ x + z
+// }
+
+// var x = 1;
+// var y = 2;
+// var z = 3;
+// test(x)
+
+// console.log(x,y,z)
+
+console.log(5 ^ 3)
---
+++ visa/rover.js
@@ -0,0 +1,4 @@
+function roverMove(matrixSize, cmds) {
+
+
+}
